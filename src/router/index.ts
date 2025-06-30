0f507a28672b9952aa802490290cfb604bf0b3e8
dddale <dalehsudemo@gmail.com>
2025-06-30 17:50:20 +0800

    [fix] base URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory('/demo-dashboard/'),
+  history: createWebHistory('/demo_dashboard/'),
   routes: [
     {
       path: "/",
